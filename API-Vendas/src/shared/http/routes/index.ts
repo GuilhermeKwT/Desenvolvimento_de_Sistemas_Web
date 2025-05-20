9a5d79bed5521b290a4eb9c6d9782a2524d373df
Guilherme Kwaczynski <139267201+GuilhermeKwT@users.noreply.github.com>
2025-05-20 17:46:18 -0300

    Add files via upload

--- API-Vendas/src/shared/http/routes/index.ts
+++ API-Vendas/src/shared/http/routes/index.ts
@@ -1,13 +1,11 @@
-import productsRouter from "@modules/products/routes/products.routes";
-import passwordRouter from "@modules/users/routes/password.routes";
-import sessionsRouter from "@modules/users/routes/sessions.routes";
-import usersRouter from "@modules/users/routes/users.routes";
-import { Router } from "express";
+import { response, Router } from "express";
+import { request } from "http";
 
 const routes = Router();
-routes.use('/products', productsRouter);
-routes.use('/users', usersRouter);
-routes.use('/sessions', sessionsRouter);
-routes.use('/password', passwordRouter);
+
+routes.get('/', (request, response) => {
+    response.json({message : 'Hello Dev'});
+    return;
+});
 
 export default routes;
